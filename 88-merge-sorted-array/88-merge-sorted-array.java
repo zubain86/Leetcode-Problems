3aa74a3a6dd34fd9a6c19a3ea6c6c387ddcfbf1f
Zubain <96322986+zubain86@users.noreply.github.com>
2022-06-14 12:32:00 +0530

    Time: 2 ms (14.17%), Space: 43.2 MB (13.63%) - LeetHub

---
+++ 88-merge-sorted-array/88-merge-sorted-array.java
@@ -0,0 +1,21 @@
+class Solution {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+ArrayList<Integer> list = new ArrayList<>();
+        int i;
+        for(i=0;i<m;i++)
+        {
+        
+                list.add(nums1[i]);
+}
+        for(i=0;i<n;i++)
+        {
+                list.add(nums2[i]);
+
+}
+        Collections.sort(list);
+        for(i=0;i<list.size();i++)
+        {
+            nums1[i] = list.get(i);
+}
+    }
+}
