c08a54fb01773d7c3103e9fd5f88ec1ea3b11e2a
Zubain <96322986+zubain86@users.noreply.github.com>
2022-07-30 21:12:12 +0530

    Time: 17 ms (12.09%), Space: 40.5 MB (76.13%) - LeetHub

---
+++ 509-fibonacci-number/509-fibonacci-number.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int fib(int n) {
+        return fibo(n);
+        
+        
+    }
+    public int fibo(int n)
+    {
+        if(n==0)
+            return 0;
+        if(n==1)
+            return 1;
+        else
+            return fibo(n-1)+fibo(n-2);
+    }
+}
