ad259eed01fd7f94ae7de9d94d264aadc4efdf3c
Zubain <96322986+zubain86@users.noreply.github.com>
2022-08-21 08:00:28 +0530

    Time: 0 ms (100.00%), Space: 42 MB (49.40%) - LeetHub

---
+++ 28-implement-strstr/28-implement-strstr.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int strStr(String haystack, String needle) {
+        if(needle.length()==0)
+        {
+            return 0;
+        }
+        if(haystack.contains(needle))
+            return haystack.indexOf(needle);
+        else
+            return -1;
+        
+    }
+}
