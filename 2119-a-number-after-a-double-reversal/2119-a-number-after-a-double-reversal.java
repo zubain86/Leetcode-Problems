6689bccb23a72b6e34be952fb8f76d7710ecaa14
Zubain <96322986+zubain86@users.noreply.github.com>
2022-10-06 09:22:00 +0530

    Time: 0 ms (100.00%), Space: 40.7 MB (62.81%) - LeetHub

--- 2119-a-number-after-a-double-reversal/2119-a-number-after-a-double-reversal.java
+++ 2119-a-number-after-a-double-reversal/2119-a-number-after-a-double-reversal.java
@@ -1,18 +1,11 @@
 class Solution {
     public boolean isSameAfterReversals(int num) {
-        String s = Integer.toString(num);
-        StringBuilder m = new StringBuilder(s);
-        m.reverse();
-        String rev = m.toString();
-        int num1 = Integer.parseInt(rev);
-        String revv = Integer.toString(num1);
-        StringBuilder p = new StringBuilder(revv);
-        p.reverse();
-        String drev = p.toString();
-        if(s.equals(drev))
+     
+        if(num==0)
             return true;
-        else
+        else if(num%10==0)
             return false;
-        
+        else
+            return true;
     }
 }
