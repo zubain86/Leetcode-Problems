de0d43d06548c0970d49126e915354625812ba12
Zubain <96322986+zubain86@users.noreply.github.com>
2022-10-29 19:39:00 +0530

    Time: 8 ms (24.91%), Space: 43.3 MB (29.21%) - LeetHub

--- 0067-add-binary/0067-add-binary.java
+++ 0067-add-binary/0067-add-binary.java
@@ -1,94 +1,10 @@
+import java.math.BigInteger;
 class Solution {
     public String addBinary(String a, String b) {
         
-        int l1 = a.length()-1;
-        int l2 = b.length()-1;
-        String carry = "0";
-        String m = "";
-        while(l1>=0 && l2>=0)
-        {
-            char ch1 = a.charAt(l1);
-            char ch2 = b.charAt(l2);
-            if(ch1=='1' && ch2=='1' && carry.equals("1"))
-            {
-                m = "1" + m;
-                carry = "1";
-            }
-           else if(ch1=='1' && ch2=='1' && carry.equals("0"))
-            {
-                m = "0" + m;
-                carry = "1";
-            }
-             else if(ch1=='0' && ch2=='0' && carry.equals("1"))
-            {
-                m = "1" + m;
-                carry = "0";
-            }
-             else if(ch1=='0' && ch2=='0' && carry.equals("0"))
-            {
-                m = "0" + m;
-                carry = "0";
-            }
-             else if(carry.equals("0"))
-            {
-                m = "1" + m;
-                carry = "0";
-            }
-             else if(carry.equals("1"))
-            {
-                m = "0" + m;
-                carry = "1";
-            }
-            l1--;
-            l2--;
-        }
-            while(l1>=0)
-            {
-                char ch = a.charAt(l1);
-                if(ch=='0' && carry.equals("0"))
-                {
-                    m = "0" + m;
-                    carry = "0";
-                }
-               else if(ch=='1' && carry.equals("1"))
-                {
-                    m = "0" + m;
-                    carry = "1";
-                }
-                else
-                {
-                    m = "1" + m;
-                    carry = "0";
-     
-                }
-                 l1--;   
-            }
-        
-            while(l2>=0)
-            {
-                char ch = b.charAt(l2);
-                if(ch=='0' && carry.equals("0"))
-                {
-                    m = "0" + m;
-                    carry = "0";
-                }
-               else if(ch=='1' && carry.equals("1"))
-                {
-                    m = "0" + m;
-                    carry = "1";
-                }
-                else
-                {
-                    m = "1" + m;
-                    carry = "0";
-     
-                }
-                l2--;
-                    
-            }
-        
-        if(carry.equals("1"))
-            m = "1" + m;
-        return m;
-    }
-}
+        BigInteger b1 = new BigInteger(a,2);
+        BigInteger b2 = new BigInteger(b,2);
+        BigInteger sum = b1.add(b2);       
+        return sum.toString(2);
+      
+}}
